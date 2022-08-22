6ff01caba21589129c22bd533c7997117173998d
Inzamum-Ul-Haque <inzamum.inu@gmail.com>
2022-08-22 20:58:10 +0600

    selection of list and added a file called utils

---
+++ js/app.js
@@ -0,0 +1,20 @@
+document
+  .getElementById("btn-select")
+  .addEventListener("click", function (event) {
+    const selectedPlayerList = document.getElementById("selected-player-list");
+    if (selectedPlayerList.childElementCount >= 5) {
+      alert("You cannot add more than 5 players");
+      return;
+    }
+
+    if (event.target.innerText === "Select") {
+      event.target.classList.add("disabled");
+    }
+
+    const playerName = event.target.parentNode.firstElementChild.innerText;
+
+    //create new li then add it to the list
+    const li = document.createElement("li");
+    li.innerText = playerName;
+    selectedPlayerList.appendChild(li);
+  });
